fa45a718da6e5c976c53e79b1236f7ee1e2221a0
winmoh <mohamedaouinti789@gmail.com>
2023-11-14 19:27:40 +0100

    basic project geneted by spring initializer

---
+++ src/test/java/com/chat/app/realtimechat/RealTimeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.app.realtimechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
